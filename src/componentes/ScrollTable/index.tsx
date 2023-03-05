b71d760ac8d4d0dcc5e19ee295dbe9e9b7a95a7d
Flavio Cardoso <flavioc41@gmail.com>
2023-03-05 08:04:26 -0300

    remoneando e verificando alguns bugs no scrolltable

--- src/componentes/ScrollTable/index.tsx
+++ src/componentes/ScrollTable/index.tsx
@@ -1,21 +1,29 @@
-import React, { useRef, MouseEvent, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
+import style from "./ScrollTable.module.scss";
 
 interface Props {
-    refDiv: React.RefObject<HTMLDivElement>
+    titulos: React.ReactNode,
+    conteudo: React.ReactNode,
 }
 
-const ButtonScrollTable: React.FC<Props> = ({ refDiv }: Props) => {
+const ScrollTable: React.FC<Props> = ({ titulos, conteudo }: Props) => {
     const refLeft = useRef<HTMLButtonElement>(null);
     const refRight = useRef<HTMLButtonElement>(null);
+    const refDiv = useRef<HTMLDivElement>(null);
     let scrollingLeft: any;
+    let speedLeft = 1;
+    let speedRight = 1;
+
     // mouse enter
-    const scrollLeftIn = (e: MouseEvent) => {
+    const scrollLeftIn = (e: any) => {
+        e = e || window.event;
         e.preventDefault();
         const div = refDiv.current;
         const left = refLeft.current;
         const rigth = refRight.current;
         if (div && left && rigth) {
             scrollingLeft = setInterval(() => {
+                left.style.backgroundColor = '#0000001f';
                 div.scrollLeft -= 1;
                 if (div.scrollLeft === 0) {
                     left.style.display = 'none';
@@ -23,16 +31,31 @@ const ButtonScrollTable: React.FC<Props> = ({ refDiv }: Props) => {
                 if (div.scrollLeft > 0) {
                     rigth.style.display = 'block';
                 }
-            }, 10 / (div.offsetLeft - e.pageX) ); // velocidade
+            }, 10 / ((speedLeft === 0) ? 1 : speedLeft)); // velocidade
         }
     }
+    // mouse leave
+    const scrollLeftOut = (e: any) => {
+        e = e || window.event;
+        e.preventDefault();
+        const left = refLeft.current;
+        left?.style.removeProperty("background-color");
+        speedLeft = 1;
+        clearInterval(scrollingLeft);
+    }
+
     // mouse down
-    const scrollLeftDown = (e: MouseEvent) => {
+    const scrollLeftDown = (e: any) => {
+        e = e || window.event;
         e.preventDefault();
         const div = refDiv.current;
         const left = refLeft.current;
         const rigth = refRight.current;
         if (div && left && rigth) {
+            speedLeft = div.offsetLeft - e.pageX;
+            clearInterval(scrollingLeft);
+            scrollLeftIn(e);
+            console.log(speedLeft);
             div.scrollLeft -= 10;
             if (div.scrollLeft === 0) {
                 left.style.display = 'none';
@@ -42,52 +65,58 @@ const ButtonScrollTable: React.FC<Props> = ({ refDiv }: Props) => {
             }
         }
     }
-    // mouse leave
-    const scrollLeftOut = (e: MouseEvent) => {
-        e.preventDefault();
-        clearInterval(scrollingLeft);
-    }
+    
     // mouse enter
-    const scrollRightIn = (e: MouseEvent) => {
+    const scrollRightIn = (e: any) => {
+        e = e || window.event;
         e.preventDefault();
         const div = refDiv.current;
         const right = refRight.current;
         const left = refLeft.current;
         if (div && right && left) {
             scrollingLeft = setInterval(() => {
+                right.style.backgroundColor = '#0000001f';
                 div.scrollLeft += 1;
                 if (div.scrollLeft > 0) {
                     left.style.display = 'block';
                 }
-
-                if (div.offsetWidth < (div.scrollLeft - 15)) {
+                if (div.offsetWidth >= div.scrollWidth - div.scrollLeft) {
                     right.style.display = 'none';
                 }
-            }, 10 / (div.offsetWidth + div.offsetLeft - 10 - e.pageX) ); // velocidade
+            }, 10 / ((speedRight === 0) ? 1 : speedRight)); // velocidade
         }
     }
+    // mouse leave
+    const scrollRightOut = (e: any) => {
+        e = e || window.event;
+        e.preventDefault();
+        const right = refRight.current;
+        right?.style.removeProperty("background-color");
+        speedRight = 1;
+        clearInterval(scrollingLeft);
+    }
     // mouse down
-    const scrollRightDown = (e: MouseEvent) => {
+    const scrollRightDown = (e: any) => {
+        e = e || window.event;
         e.preventDefault();
         const div = refDiv.current;
         const left = refLeft.current;
         const right = refRight.current;
         if (div && right && left) {
+            speedRight = e.pageX - div.offsetWidth;
+            clearInterval(scrollingLeft);
+            scrollRightIn(e);
+            console.log(speedRight);
             div.scrollLeft += 10;
             if (div.scrollLeft > 0) {
                 left.style.display = 'block';
             }
-
-            if (div.offsetWidth <= (div.scrollLeft - 15)) {
+            if (div.offsetWidth >= div.scrollWidth - div.scrollLeft) {
                 right.style.display = 'none';
             }
         }
     }
-    // mouse leave
-    const scrollRightOut = (e: MouseEvent) => {
-        e.preventDefault();
-        clearInterval(scrollingLeft);
-    }
+    
     // mudando o style
     useEffect(() => {
         const div = refDiv.current;
@@ -97,6 +126,9 @@ const ButtonScrollTable: React.FC<Props> = ({ refDiv }: Props) => {
             if (div.scrollLeft === 0) {
                 left.style.display = 'none';
             }
+            div.style.overflow = 'scroll';
+            div.style.marginBottom = '1rem';
+            div.style.whiteSpace = 'nowrap';
             let position = 'fixed';
             let mouseLeft = 'w-resize';
             let mouseRight = 'e-resize';
@@ -115,7 +147,7 @@ const ButtonScrollTable: React.FC<Props> = ({ refDiv }: Props) => {
             rigth.style.position = position;
             rigth.style.cursor = mouseRight;
             rigth.style.top = `${positionTop}px`;
-            rigth.style.left = `${positionRight - 10}px`;
+            rigth.style.left = `${positionRight}px`;
             rigth.style.height = `${positionHeight}px`;
             rigth.style.width = `${tamanho}rem`;
         }
@@ -133,7 +165,18 @@ const ButtonScrollTable: React.FC<Props> = ({ refDiv }: Props) => {
             onMouseLeave={scrollRightOut}
             onMouseDown={scrollRightDown}
         ></button>
+        <div ref={refDiv} className={style.TabelaStyle}>
+            <table>
+                <thead>
+                    { titulos }
+                </thead>
+                <tbody>
+                    { conteudo }
+                </tbody>
+            </table>
+        </div>
         </>
     );
 }
-export default ButtonScrollTable;
+
+export default ScrollTable;
